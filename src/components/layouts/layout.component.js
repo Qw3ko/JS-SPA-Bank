047e0814a9de546d1009fe125697bb44fbd39bdc
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-11-12 19:14:55 +0500

    updated layout

--- src/components/layouts/layout.component.js
+++ src/components/layouts/layout.component.js
@@ -1,31 +1,35 @@
-import renderService from "@/core/services/render.service"
+import renderService from '@/core/services/render.service'
 import template from './layout.template.html'
 import styles from './layout.module.scss'
-import { $Q } from "@/core/qwquery/qwquery.lib"
-import { Header } from "./header/header.component"
-import ChildComponent from "@/core/component/child.component"
+import { $Q } from '@/core/qwquery/qwquery.lib'
+import { Header } from './header/header.component'
+import ChildComponent from '@/core/component/child.component'
+import { Notification } from './notification/notification.component'
 
 export class Layout extends ChildComponent {
-    constructor({router, children}) {
-        super()
-        
-        this.router = router
-        this.children = children
-    }
+	constructor({ router, children }) {
+		super()
 
+		this.router = router
+		this.children = children
+	}
 
-    render() {
-        this.element = renderService.htmlToElement(template, [], styles)
+	render() {
+		this.element = renderService.htmlToElement(template, [Notification], styles)
 
-        const mainElement = $Q(this.element).find('main')
+		const mainElement = $Q(this.element).find('main')
 
-        const contentContainer = $Q(this.element).find('#content')
-        contentContainer.append(this.children)
+		const contentContainer = $Q(this.element).find('#content')
+		contentContainer.append(this.children)
 
-        mainElement
-        .before(new Header().render())
-        .append(contentContainer.element)
+		mainElement
+			.before(
+				new Header({
+					router: this.router
+				}).render()
+			)
+			.append(contentContainer.element)
 
-        return this.element
-    }
+		return this.element
+	}
 }
