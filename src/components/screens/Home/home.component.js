96cfbee380cbd82dfd4f970b27f366640e8add9d
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-11-15 20:33:58 +0500

    updated home screen

--- src/components/screens/Home/home.component.js
+++ src/components/screens/Home/home.component.js
@@ -2,16 +2,17 @@ import { BaseScreen } from "@/core/component/base-screen.component";
 import renderService from "@/core/services/render.service";
 import template from './home.template.html'
 import styles from './home.module.scss'
-import { $Q } from "@/core/qwquery/qwquery.lib";
+import { CardInfo } from "./card-info/card-info.component";
+import { Actions } from "./actions/actions.component";
 
-export class Home extends BaseScreen { 
+export class Home extends BaseScreen {
+    
     constructor () {
         super( {title : 'Home'} )
     }
-    render() {
-        const element = renderService.htmlToElement(template, [], styles)
 
-        $Q(element).find('h1').css('color', 'black')
+    render() {
+        const element = renderService.htmlToElement(template, [CardInfo, Actions], styles)
 
         return element
     }
