394053b2f06f7b10c37e5278a61636f6dfe76bf2
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-11-15 20:35:25 +0500

    updated card info component

--- src/components/screens/Home/card-info/card-info.component.js
+++ src/components/screens/Home/card-info/card-info.component.js
@@ -1,11 +1,105 @@
 import ChildComponent from '@/core/component/child.component'
-import renderService from "@/core/services/render.service";
+import renderService from '@/core/services/render.service'
 import template from './card-info.template.html'
 import styles from './card-info.module.scss'
+import { CardService } from '@/api/card.service'
+import { Store } from '@/core/store/store'
+import { $Q } from '@/core/qwquery/qwquery.lib'
+import { formatCardNumber } from '@/utils/format/format-card-number'
+import { formatToCurrency } from '@/utils/format/format-to-currency'
+import { BALANCE_UPDATED } from '@/constants/event.constants'
 
-export class CardInfo extends ChildComponent { 
-    render() {
-        this.element = renderService.htmlToElement(template, [], styles)
-        return this.element
-    }
+const CODE = '*****'
+
+export class CardInfo extends ChildComponent {
+	constructor() {
+		super()
+
+		this.store = Store.getInstance()
+		this.cardService = new CardService()
+
+		this.element = renderService.htmlToElement(template, [], styles)
+
+		this.#addListeners()
+	}
+
+	#addListeners() {
+		document.addEventListener(BALANCE_UPDATED, this.#onBalanceUpdated)
+	}
+
+	#removeListeners() {
+		document.removeEventListener(BALANCE_UPDATED, this.#onBalanceUpdated)
+	}
+
+	#onBalanceUpdated = () => {
+		this.fetchData()
+	}
+
+	destroy() {
+		this.#removeListeners()
+	}
+
+	#copyCardNumber(e) {
+		navigator.clipboard.writeText(e.target.innerText).then(() => {
+			e.target.innerText = 'Card number copied!'
+			setTimeout(() => {
+				e.target.innerText = formatCardNumber(this.card.number)
+			}, 2000)
+		})
+	}
+
+	#toggleCvc(cardCvcElement) {
+		const text = cardCvcElement.text()
+		text === CODE
+			? cardCvcElement.text(this.card.cvc)
+			: cardCvcElement.text(CODE)
+	}
+
+	fillElements() {
+		$Q(this.element).html(
+			renderService.htmlToElement(template, [], styles).innerHTML
+		)
+
+		$Q(this.element)
+			.findAll(':scope > div')
+			.forEach(child => {
+				child.addClass('fade-in')
+			})
+
+		$Q(this.element)
+			.find('#card-number')
+			.text(formatCardNumber(this.card.number))
+            .click(this.#copyCardNumber.bind(this))
+
+		$Q(this.element).find('#card-expire-date').text(this.card.expireDate)
+
+		const cardCvcElement = $Q(this.element).find('#card-cvc')
+		cardCvcElement.text(CODE).css('width', '44px')
+
+		$Q(this.element)
+			.find('#toggle-cvc')
+			.click(this.#toggleCvc.bind(this, cardCvcElement))
+
+		$Q(this.element)
+			.find('#card-balance')
+			.text(formatToCurrency(this.card.balance))
+	}
+
+	fetchData() {
+		this.cardService.byUser(data => {
+			if (data?.id) {
+				this.card = data
+				this.fillElements()
+				this.store.updateCard(data)
+			} else {
+				this.store.updateCard(null)
+			}
+		})
+	}
+
+	render() {
+		if (this.store.state.user) this.fetchData()
+
+		return this.element
+	}
 }
