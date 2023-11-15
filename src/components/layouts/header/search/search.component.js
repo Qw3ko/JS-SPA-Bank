cfc9dbcefda596f2289cbace011ff27f71718d0a
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-11-15 20:34:32 +0500

    updated search component

--- src/components/layouts/header/search/search.component.js
+++ src/components/layouts/header/search/search.component.js
@@ -1,21 +1,63 @@
 import ChildComponent from '@/core/component/child.component'
-import renderService from "@/core/services/render.service";
+import renderService from '@/core/services/render.service'
 import template from './search.template.html'
 import styles from './search.module.scss'
-import { $Q } from '@/core/qwquery/qwquery.lib';
+import { $Q } from '@/core/qwquery/qwquery.lib'
+import { UserService } from '@/api/user.service'
+import { UserItem } from '@/components/ui/user-item/user-item.component'
+import { debounce } from '@/utils/debounce'
 
-export class Search extends ChildComponent { 
-    render() {
-        this.element = renderService.htmlToElement(template, [], styles)
+export class Search extends ChildComponent {
+	constructor() {
+		super()
 
-        $Q(this.element)
-        .find('input')
-        .input({
-            type: 'search',
-            name: 'search',
-            placeholder: 'Search contacts...'
-        })
-        
-        return this.element
-    }
+		this.userService = new UserService()
+	}
+
+	#handleSearch = async event => {
+		const searchTerm = event.target.value
+		const searchResultElement = $Q(this.element).find('#search-results')
+
+		if (!searchTerm) {
+			searchResultElement.html('')
+			return
+		}
+
+		await this.userService.getAll(searchTerm, users => {
+			searchResultElement.html('')
+
+			users.forEach((user, index) => {
+				const userItem = new UserItem(user, true, () => {
+					searchResultElement.html('')
+				}).render()
+
+				$Q(userItem)
+					.addClass(styles.item)
+					.css('transition-delay', `${index * 0.1}s`)
+
+				searchResultElement.append(userItem)
+
+				setTimeout(() => {
+					$Q(userItem).addClass(styles.visible)
+				}, 50)
+			})
+		})
+	}
+
+	render() {
+		this.element = renderService.htmlToElement(template, [], styles)
+
+		const debouncedHandleSearch = debounce(this.#handleSearch, 300)
+
+		$Q(this.element)
+			.find('input')
+			.input({
+				type: 'search',
+				name: 'search',
+				placeholder: 'Search contacts...'
+			})
+			.on('input', debouncedHandleSearch)
+
+		return this.element
+	}
 }
