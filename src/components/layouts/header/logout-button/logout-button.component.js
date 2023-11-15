d0df0f3da73a0e6c28dc4f2248a84a7f635a49aa
Qw3ko <bannikov.arseniyy@rambler.ru>
2023-11-15 20:33:03 +0500

    updated logout button component

--- src/components/layouts/header/logout-button/logout-button.component.js
+++ src/components/layouts/header/logout-button/logout-button.component.js
@@ -1,24 +1,30 @@
 import ChildComponent from '@/core/component/child.component'
-import renderService from "@/core/services/render.service";
+import renderService from '@/core/services/render.service'
 import template from './logout-button.template.html'
 import styles from './logout-button.module.scss'
-import { $Q } from '@/core/qwquery/qwquery.lib';
+import { $Q } from '@/core/qwquery/qwquery.lib'
+import { Store } from '@/core/store/store'
 
 export class LogoutButton extends ChildComponent {
-    
-    constructor({router}) {
-        super()
+	constructor({ router }) {
+		super()
 
-        this.router = router
-    }
-    
-    render() {
-        this.element = renderService.htmlToElement(template, [], styles)
+		this.store = Store.getInstance()
+		this.user = this.store.state.user
 
-        $Q(this.element).find('button').click(() => {
-            this.router.navigate('/auth')
-        })
+		this.router = router
+	}
 
-        return this.element
-    }
+	render() {
+		this.element = renderService.htmlToElement(template, [], styles)
+
+		$Q(this.element)
+			.find('button')
+			.click(() => {
+				this.store.logout()
+				this.router.navigate('/auth')
+			})
+
+		return this.element
+	}
 }
